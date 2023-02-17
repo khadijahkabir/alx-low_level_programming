d300b3f5111e79b236ef68ca3054439c82171ca3
khadijahkabir <evesseacret@gmail.com>
2023-02-16 16:05:06 -0800

    4 puts.c updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
+
+
 /**
  * main - A program that prints a line with puts function
  * Return: Always 0 (Success)
  * /
-int main (void)
+ 
+
+int main(void)
+
+
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
